80e5dfdabd92f3ea5628bc5ad0d2be11b281f1ee
11702270 <11702270@student.pxl.be>
2020-08-18 21:33:20 +0200

    update splash screen
    improved the look of the spash screen

--- app/src/main/java/com/example/skitchen/fragments/HomeFragment.java
+++ app/src/main/java/com/example/skitchen/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.skitchen.fragments;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.skitchen.Meal;
@@ -30,7 +32,7 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView rvMeals;
+    private RecyclerView rvMeals;
 
     private FirebaseUser currentUser;
 
